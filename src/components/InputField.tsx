b468c9d7d4b16821a553226eab3f6a55257f272c
saumyaa03 <63736923+saumyaa03@users.noreply.github.com>
2025-08-23 13:58:48 -0400

    Make format changes

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,27 +1,28 @@
-import React, { useState } from 'react'
-import '../styles/styles.css'
+import React, { useState } from "react";
+import "../styles/styles.css";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const InputField = ({todo, setTodo} : Props) => {
-// const InputField: React.FC<Props> = ({todo, setTodo}) => { // you can use either
-
-console.log(todo);
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  // const InputField: React.FC<Props> = ({todo, setTodo}) => { // you can use either
 
   return (
-    <form className='input-form'> 
-      <input type='text' 
-      placeholder='Enter a task' 
-      value={todo}
-       onChange={
-        (e) => setTodo(e.target.value)
-       }
-      className='input-box'/>
-      <button type='submit' className='input-submit'>Go</button>
+    <form className="input-form" onSubmit={handleAdd}>
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input-box"
+      />
+      <button type="submit" className="input-submit">
+        Go
+      </button>
     </form>
-  )
-}
+  );
+};
 
 export default InputField;
