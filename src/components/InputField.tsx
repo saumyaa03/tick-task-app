e41e7907ada5282a7dc1b03f74fe18c8e04a2638
saumyaa03 <63736923+saumyaa03@users.noreply.github.com>
2025-08-23 12:09:57 -0400

    feat: add InputField component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/styles.css'
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+const InputField = ({todo, setTodo} : Props) => {
+// const InputField: React.FC<Props> = ({todo, setTodo}) => { // you can use either
+
+console.log(todo);
+
   return (
     <form className='input-form'> 
-      <input type='text' placeholder='Enter a task' className='input-box'/>
+      <input type='text' 
+      placeholder='Enter a task' 
+      value={todo}
+       onChange={
+        (e) => setTodo(e.target.value)
+       }
+      className='input-box'/>
       <button type='submit' className='input-submit'>Go</button>
     </form>
   )
 }
 
-export default InputField
+export default InputField;
